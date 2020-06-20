c3621b0a96cf4363e9b7af8f2b2a9c7fbe3ac2d1
AbhishekSanthoshJaya <abhisheksj35@gmail.com>
2020-06-20 07:58:07 -0400

    Added CoreDataHelper class containing function to add a new SlickNotes object into core data

---
+++ SlickNotes/SlickNotesCoreDataHelper.swift
@@ -0,0 +1,53 @@
+//
+//  SlickNotesCoreDataHelper.swift
+//  SlickNotes
+//
+//  Created by user166476 on 6/20/20.
+//  Copyright © 2020 Quasars. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class SlickNotesCoreDataHelper
+{
+    private(set) static var count: Int = 0
+    
+    static func createNoteInCoreData(
+        noteToBeCreated:          SlickNotes,
+        intoManagedObjectContext: NSManagedObjectContext) {
+        
+        // Let’s create an entity and new note record
+        let noteEntity = NSEntityDescription.entity(
+            forEntityName: "Note",
+            in:            intoManagedObjectContext)!
+        
+        let newNoteToBeCreated = NSManagedObject(
+            entity:     noteEntity,
+            insertInto: intoManagedObjectContext)
+
+        newNoteToBeCreated.setValue(
+            noteToBeCreated.noteId,
+            forKey: "noteId")
+        
+        newNoteToBeCreated.setValue(
+            noteToBeCreated.noteTitle,
+            forKey: "noteTitle")
+        
+        newNoteToBeCreated.setValue(
+            noteToBeCreated.noteText,
+            forKey: "noteText")
+        
+        newNoteToBeCreated.setValue(
+            noteToBeCreated.noteTimeStamp,
+            forKey: "noteTimeStamp")
+        
+        do {
+            try intoManagedObjectContext.save()
+            count += 1
+        } catch let error as NSError {
+            // TODO error handling
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+    }
+}
