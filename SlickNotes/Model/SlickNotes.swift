10883e9a053b450870061634c9e6e809b29f4735
prana <prakashrana9847067614@gmail.com>
2020-06-20 16:33:56 -0400

    Added latitude and longitude in notes class and core model

--- SlickNotes/Model/SlickNotes.swift
+++ SlickNotes/Model/SlickNotes.swift
@@ -13,18 +13,25 @@ class SlickNotes {
     private(set) var noteTitle     : String
     private(set) var noteText      : String
     private(set) var noteTimeStamp : Int64
+    private(set) var latitude : String
+    private(set) var longitude : String
+
     
-    init(noteTitle:String, noteText:String, noteTimeStamp:Int64) {
+    init(noteTitle:String, noteText:String, noteTimeStamp:Int64, latitude: String, longitude: String) {
         self.noteId        = UUID()
         self.noteTitle     = noteTitle
         self.noteText      = noteText
         self.noteTimeStamp = noteTimeStamp
+        self.latitude = latitude
+        self.longitude = longitude
     }
 
-    init(noteId: UUID, noteTitle:String, noteText:String, noteTimeStamp:Int64) {
+    init(noteId: UUID, noteTitle:String, noteText:String, noteTimeStamp:Int64, latitude: String, longitude: String) {
         self.noteId        = noteId
         self.noteTitle     = noteTitle
         self.noteText      = noteText
         self.noteTimeStamp = noteTimeStamp
+        self.latitude = latitude
+       self.longitude = longitude
     }
 }
